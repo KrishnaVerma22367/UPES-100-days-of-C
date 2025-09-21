28bd0485a480694755540c679da7bed3807b2c21
KrishnaVerma22367 <Krishna.22367@stu.upes.ac.in>
2025-09-21 16:19:38 +0530

    codes
    Day 6 of 100 days of code

---
+++ Day 6/Q11.c
@@ -0,0 +1,21 @@
+// Q11: Write a program to input an integer and check whether it is even or odd using if–else.
+
+#include<stdio.h>
+
+int main() {
+
+    int num;
+
+    printf("Enter an integer:");
+    scanf("%d", &num);
+
+    if( num % 2 == 0) {
+        printf("%d is an positive integer\n", num);
+    }
+    else {
+        printf("%d is an negitive integer\n", num);
+    }
+
+return 0;
+
+}
